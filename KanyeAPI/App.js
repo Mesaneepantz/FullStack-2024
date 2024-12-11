9edfb35748c1260c2d67deb0463f7c08e3906864
mesaneeanurit@gmail.com <your-email@example.com>
2024-12-11 10:01:25 +0700

    Update KanyeAPI Project

---
+++ KanyeAPI/App.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+
+const axios = require('axios');
+
+app.get('/kanye', async (req, res) => {
+   try {
+       const url = 'https://api.kanye.rest/'; 
+       const response = await axios.get(url);
+       console.log(response)
+       res.json(response.data); // Send the fetched data as a response
+   } catch (error) {
+       res.status(500).send('Error fetching data');
+   }
+});
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000!")
+})
